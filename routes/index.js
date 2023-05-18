4392ca4cecfca9f615f7fc90a7f0798c69513eb3
Dahian Catalina NuÑEZ Gutierrez <dahiancatalinan@gmail.com>
2023-05-18 16:40:39 -0500

    Agregando archivo index.js a las rutas
    Se agregó un nuevo archivo llamado 'index.js' en la carpeta de rutas.
    El modo del archivo se estableció en 100644.
    El índice anterior del archivo era '0000000' y el nuevo índice es 'c99cd6c'.
    Antes de esta adición, el archivo no existía ('dev/null').
    Se importó el módulo Express en el archivo.
    Se creó un enrutador utilizando el método express.Router().
    Se comentaron las líneas que importaban todas las rutas desde un archivo llamado 'ejemploRoutes'.
    Se comentaron las líneas que agregaban todas las rutas al enrutador principal.
    Se exportó el enrutador como módulo para que pueda ser utilizado en otros archivos.

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// Importar todas las rutas
+//const ejemploRoutes = require('./ejemploRoutes');
+
+// Agregar todas las rutas al enrutador principal
+//router.use('/ejemplo', ejemploRoutes);
+
+// Exportar el enrutador
+module.exports = router;
